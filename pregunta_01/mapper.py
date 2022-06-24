a497dc7b6c763e4d7a169b2ab788908a66e49abb
nriost <95105995+nriost@users.noreply.github.com>
2022-06-24 12:37:30 -0500

    Add files via upload

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -1,3 +1,18 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+#
+# >>> Escriba el codigo del mapper a partir de este punto <<<
+#
+#! /usr/bin/python3
+import sys 
+if __name__ == "__main__":
+
+    ##
+    ## itera sobre cada linea de codigo recibida
+    ## a traves del flujo de entrada
+    ##
+    for line in sys.stdin:
+       fields=line [0:-1].split(',')
+       sys.stdout.write("{}\t1\n".format(fields[2]))     
